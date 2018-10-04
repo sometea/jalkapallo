0190ded6a5415d7f18251d6ddba023f0e8569b39
David Dasenbrook <david.dasenbrook@tngtech.com>
2018-10-04 20:06:12 +0200

    save sample data into new table

--- src/createTable.ts
+++ src/createTable.ts
@@ -1,4 +1,5 @@
 import AWS from 'aws-sdk';
+import uuidv4 from 'uuid';
 
 AWS.config.update({
     region: "eu-west-1",
@@ -14,7 +15,7 @@ dynamoDb.createTable({
         { AttributeName: 'title', KeyType: 'RANGE' },
     ],
     AttributeDefinitions: [
-        { AttributeName: 'id', AttributeType: 'N' },
+        { AttributeName: 'id', AttributeType: 'S' },
         { AttributeName: 'title', AttributeType: 'S' },
     ],
     ProvisionedThroughput: {
@@ -26,5 +27,28 @@ dynamoDb.createTable({
         console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
     } else {
         console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+        saveSampleData();
     }
 });
+
+function saveSampleData() {
+    const documentClient = new AWS.DynamoDB.DocumentClient();
+    const items = [
+        { title: 'The badgers are back.', body: 'All the badgers have been seen again.' },
+        { title: 'Hellow badgers.', body: 'The badgers are really good people.' },
+        { title: 'Tea', body: 'It\'s nice to finally have some tea together.' },
+    ];
+    for (let item of items) {
+        const params = {
+            TableName: 'Articles',
+            Item: { ...item, id:  uuidv4() },
+        };
+        documentClient.put(params, (err, data) => {
+            if (err) {
+                console.log('Unable to add article "' + item.title, '": ' + JSON.stringify(err, null, 2));
+            } else {
+                console.log('Successfully added article "' + item.title + '"');
+            }
+        })
+    }
+}
