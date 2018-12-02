89c0beb2441be95e9f4cb3784f5d08ff3de469f8
David Dasenbrook <david.dasenbrook@tngtech.com>
2018-12-02 14:16:39 +0100

    update lambda deployment script to use s3 bucket instead

--- uploadLambdaPackage.sh
+++ uploadLambdaPackage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Update the lambda function by uploading the lambda package
-aws lambda update-function-code --function-name Jalkapallo --zip-file fileb://deployment.zip --publish
+aws s3 cp deployment.zip s3://jalkapallo-deployment
+aws lambda update-function-code --function-name JalkapalloLambda --s3-bucket jalkapallo-deployment --s3-key deployment.zip --publish
